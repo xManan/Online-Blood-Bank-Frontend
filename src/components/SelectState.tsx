1aa2c4fe9c0e2b8c2ab3964af517a4491d45114b
xManan <mananchawla10@gmail.com>
2023-02-13 07:14:34 +0530

    infinite loading fixed

--- src/components/SelectState.tsx
+++ src/components/SelectState.tsx
@@ -8,15 +8,22 @@ interface SelectStateProps {
 
 function SelectState(props: SelectStateProps) {
     const [states, setStates] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
 
-    const fetchData = () => {
-        return fetch("http://localhost:6969/api/states")
-            .then((response) => response.json())
-            .then((data) => { 
-                setStates(data.states.map((state: { _id: any, state: string }) => state.state)) 
-                setLoading(false)
-            });
+    const fetchData = async () => {
+        console.log("fd")
+        setLoading(true)
+        try{
+            let response = await fetch("http://localhost:6969/api/states")
+            if (response.status >= 200 && response.status <= 299) {
+                let data = await response.json()
+                setStates(data.states.map((state: { _id: any, state: string }) => state.state))
+            }
+            setLoading(false)
+        } catch(e) {
+            console.log("oops!")
+            setLoading(false)
+        }
     }
 
     useEffect(() => {
@@ -26,8 +33,8 @@ function SelectState(props: SelectStateProps) {
     return (
         <Form.Select disabled={loading} value={props.value} onChange={(e) => props.setState(e.target.value)}>
             <option value="">{loading ? 'loading' : 'Select State'}</option>
-            {states.map((state,idx) => (
-                <option key={'state'+idx}>{state}</option>
+            {states.map((state, idx) => (
+                <option key={'state' + idx}>{state}</option>
             ))}
         </Form.Select>
     )
